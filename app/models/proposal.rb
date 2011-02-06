4f01b56a995acbbb8bdef6123b962a0c152759f4
Ryan Hsu <ryanlives@gmail.com>
2011-02-05 18:05:51 -0800

    test commit

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,5 +2,5 @@ class Proposal < ActiveRecord::Base
   belongs_to :user
   has_many :documents
   
-  accepts_nested_attributes_for :documents, :allow_destroy => true
+  accepts_nested_attributes_for :documents, :allow_destroy => true 
 end
