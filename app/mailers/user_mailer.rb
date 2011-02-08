65a8819eb8367cfd06e5de8f2d39173224ab2551
Ryan Hsu <ryanlives@gmail.com>
2011-02-07 21:11:19 -0800

    added host option (when deployed, this needs to be in config/application.rb)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default :from => "noreply@prrp.com"
   
   def password_reset_instructions(user)
-    @edit_password_reset_url = edit_password_reset_url(user.perishable_token)
+    @edit_password_reset_url = edit_password_reset_url(user.perishable_token, :host => "localhost:3000")
 
     mail(:to => user.email,
          :subject => "PRRP - Reset Your Password")
