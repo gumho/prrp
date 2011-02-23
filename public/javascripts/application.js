597dcf18c3d84037aa7b44cabb9caf53313797f3
Ryan Hsu <ryanlives@gmail.com>
2011-02-22 20:51:31 -0800

    added comments for readability

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+// Proposal#new
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
