fa52edf40fc890082968680794e0a8bcdbbee64f
Ryan Hsu <ryanlives@gmail.com>
2011-02-24 11:35:04 -0800

    corrected redirection from new comment -> proposal

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.create(params[:comment])
     if @comment.save
-      redirect_to(user_proposal_path(params[:comment][:user_id], params[:comment][:proposal_id]), :notice => 'Proposal was successfully created.')
+      redirect_to(user_proposal_path(@comment.proposal.user, @comment.proposal), :notice => 'Proposal was successfully created.')
     else
       render :action => "new"
     end
