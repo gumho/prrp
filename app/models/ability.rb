47752ebcad01c103453700eccd757eaac9aa2b1f
Ryan Hsu <ryanlives@gmail.com>
2011-02-26 15:50:01 -0800

    minor update

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,15 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.role.name == "admin"
+    if user.role.name == 'admin'
       can :manage, :all
-    elsif user.role.name == "applicant"
+    elsif user.role.name == 'applicant'
       can :apply, :proposal
-    elsif user.role.name == "campus admin"
+    elsif user.role.name == 'campus admin'
       can :administer, :campus_controls
+      can :review, :proposals
+      can :elect, :proposals
+    elsif user.role.name == 'campus reviewer'
       can :review, :proposals
     end
   end
