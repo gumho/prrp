3b972abd044815acb2871fb56ad747bc7d6327ec
Ryan Hsu <ryanlives@gmail.com>
2011-02-27 14:56:01 -0800

    removed unused migration

--- db/migrate/20110227055930_add_elector_column_to_campus_winners.rb
+++
@@ -1,9 +0,0 @@
-class AddElectorColumnToCampusWinners < ActiveRecord::Migration
-  def self.up
-    add_column :campus_winners, :user_id, :integer
-  end
-
-  def self.down
-    remove_column :campus_winners, :user_id
-  end
-end
