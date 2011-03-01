7f00c118ad7c1d6e0da66539fcb6adc9198dfd47
Ryan Hsu <ryanlives@gmail.com>
2011-03-01 08:14:08 -0800

    added :manage :users ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,14 +1,22 @@
 class Ability
   include CanCan::Ability
 
+  # :manage :all - Do everything
+  # :manage :users - Add/See Users
+  # :apply :proposal - Apply proposals
+  # :review :proposals
+  # :elect :proposals
+  
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.role.name == 'admin'
       can :manage, :all
+      can :manage, :users
     elsif user.role.name == 'applicant'
       can :apply, :proposal
     elsif user.role.name == 'campus admin'
       can :administer, :campus_controls
+      can :manage, :users
       can :review, :proposals
       can :elect, :proposals
     elsif user.role.name == 'campus reviewer'
