92b2c66527ede4a6e794858aef060ec22771e3e0
Ryan Hsu <ryanlives@gmail.com>
2011-03-02 21:28:57 -0800

    put in fix for nil users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,21 +6,29 @@ class Ability
   # :apply :proposal - Apply proposals
   # :review :proposals
   # :elect :proposals
-  
+
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.role.name == 'admin'
-      can :manage, :all
-      can :manage, :users
-    elsif user.role.name == 'applicant'
-      can :apply, :proposal
-    elsif user.role.name == 'campus admin'
-      can :administer, :campus_controls
-      can :manage, :users
-      can :review, :proposals
-      can :elect, :proposals
-    elsif user.role.name == 'campus reviewer'
-      can :review, :proposals
+
+    if user.role == nil
+      can :do, :nothing
+    else
+
+      if user.role.name == 'admin'
+        can :manage, :all
+        can :manage, :users
+      elsif user.role.name == 'applicant'
+        can :apply, :proposal
+      elsif user.role.name == 'campus admin'
+        can :administer, :campus_controls
+        can :manage, :users
+        can :review, :proposals
+        can :elect, :proposals
+      elsif user.role.name == 'campus reviewer'
+        can :review, :proposals
+      else
+        can :do, :nothing
+      end
     end
   end
 end
