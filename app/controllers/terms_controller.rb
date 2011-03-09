25b943cc2bc5f5e75de10432a01552f8c2b124c8
Ryan Hsu <ryanlives@gmail.com>
2011-03-08 18:38:21 -0800

    implemented new term

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -8,6 +8,45 @@ class TermsController < ApplicationController
       :order => "begin DESC"
   end
   
+  def new
+    @term = Term.new
+  end
+  
+  def create
+    @term = Term.new(params[:term])
+    
+    if @term.save
+      redirect_to terms_path, :notice => 'Term successfully created'
+    else
+      render :action => "new"
+    end
+  end
+  
+  def destroy
+    @term = Term.find(params[:id])
+    
+    # if term is the 'current' term, destroy that
+    current = CurrentTerm.all.first
+    if current
+      if @term.id == current.id
+        current.destroy
+      end
+    end
+    
+    # lastly, destroy the term
+    @term.destroy
+    
+    redirect_to terms_path, :notice => 'Term was successfully destroyed'
+  end
+  
+  def set_current
+    
+  end
+  
+  def unset_current
+    
+  end
+  
   def activate
     term = current_term.term
     term.active = true
