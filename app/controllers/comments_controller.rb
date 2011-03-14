4f051d9c1444b98503ff3a36cf176074b3c5e488
Ryan Hsu <ryanlives@gmail.com>
2011-03-13 18:45:20 -0700

    typo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.create(params[:comment])
     if @comment.save
-      redirect_to(user_proposal_path(@comment.proposal.user, @comment.proposal), :notice => 'Proposal was successfully created.')
+      redirect_to(user_proposal_path(@comment.proposal.user, @comment.proposal), :notice => 'Comment was successfully created.')
     else
       render :action => "new"
     end
