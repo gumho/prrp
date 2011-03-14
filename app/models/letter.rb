1b7a605a7b9f0f78a19c2394b0c896bfea69153b
Ryan Hsu <ryanlives@gmail.com>
2011-03-14 00:24:12 -0700

    added validations on all columns

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,3 +1,6 @@
 class Letter < ActiveRecord::Base
   belongs_to :term
+  
+  validates_presence_of :recommender_name, :applicant_relation, :recommender_email,
+    :applicant_first_name, :applicant_last_name, :applicant_organization, :content
 end
