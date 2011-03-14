58e462219fbb550ee9572bc33479922a1f3499bd
Ryan Hsu <ryanlives@gmail.com>
2011-03-13 18:38:38 -0700

    comment behavior changed. a proposal is now 'reviewed' if 3 prrp comments have been made

--- app/models/comment_observer.rb
+++ app/models/comment_observer.rb
@@ -1,11 +1,22 @@
 class CommentObserver < ActiveRecord::Observer
   
   def after_create(comment)
-    if comment.proposal.reviewed == false
+    comments = comment.proposal.comments
+    
+    prrp_reviewer_count = 0
+    
+    comments.each do |com|
+      if com.user.role.name == 'prrp reviewer'
+        prrp_reviewer_count += 1
+      end
+    end
+    
+    if prrp_reviewer_count >= 3
       @p = comment.proposal
       @p.reviewed = true
       @p.save
     end
+
   end
   
 end
